228b1f0d0adaed2b07c00ceeae99ef14e2e3ad84
Evgenia Isaeva <evgenia.isaeva297@gmail.com>
2023-03-18 17:13:47 +0300

    News homepage

--- script.js
+++ script.js
@@ -2,8 +2,12 @@
 
 const header = document.querySelector(".header");
 const burgerBtn = header.querySelector(".menu-btn");
+const overlay = header.querySelector(".overlay");
 
-burgerBtn.addEventListener("click", () => {
+burgerBtn.addEventListener("click", toggleMenu);
+overlay.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
   header.classList.toggle("menu--open");
   document.querySelector("body").classList.toggle("overflow-hidden");
-});
+}
